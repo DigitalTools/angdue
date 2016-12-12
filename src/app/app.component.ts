469ab4a7cf245a4dda897b1175b3f337fa60db2d
Franco Cedillo <franco.cedillo@gmail.com>
2016-12-12 14:05:33 -0500

    decouple hero detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,7 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  
-}
-
-export class Hero {
-  id: number;
-  name: string;
+   
 }
 
 const HEROES: Hero[] = [
