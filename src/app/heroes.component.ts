9cb4ec3167685319851e081dcd7cc992f7a8370a
Franco Cedillo <franco.cedillo@gmail.com>
2016-12-13 01:18:20 -0500

    fix. forget to include heroes component

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.styl']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) {
+    
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+}
