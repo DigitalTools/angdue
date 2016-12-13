56890d6603d95a99ff0dd2dc2cb1ab484cf309d8
Franco Cedillo <franco.cedillo@gmail.com>
2016-12-13 01:17:44 -0500

    split the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,11 @@
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent implements OnInit {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  constructor(private heroService: HeroService) {
-    
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
+export class AppComponent {
+    title = 'Tour of Heroes';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
   imports: [
